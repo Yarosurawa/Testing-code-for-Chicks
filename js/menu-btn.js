4c76d9ee85251852841bba0863a610d952b362ed
Yarosurawa <yarosurawa@gmail.com>
2022-10-30 17:42:38 +0200

    some menues ig

--- js/menu-btn.js
+++ js/menu-btn.js
@@ -1,18 +1,28 @@
-const menu = document.querySelector('.menu')
+const menuDed = document.querySelector('.menu-ded'); const menu = document.querySelector('.menu')
 
 let isPaused = false
 
-menu.classList.add('hidden')
-menu.style.zIndex = "0"
+menuDed.classList.add('hidden'); menu.classList.add('hidden');
+menuDed.style.zIndex = "-1231231"; menu.style.zIndex = "-1231231"
+menuDed.style.display = 'none'; menu.style.display = 'none'
 
 function menuwindow( dead ) {
     if(dead) {
         isPaused = true
-        menu.classList.remove('hidden')
-        menu.style.zIndex = "9999"
+        menuDed.style.zIndex = "9999"
+        menuDed.style.display = 'flex'
+        menuDed.classList.remove('hidden')
     } else if (!dead && !isPaused){
+        menu.style.display = 'flex'
+        menu.style.zIndex = "9999"
+        requestAnimationFrame(()=>{menu.classList.remove('hidden')}) 
         isPaused = true
     } else if (!dead && isPaused){
+        menu.classList.add('hidden');
+        setTimeout(()=>{
+            menu.style.zIndex = "-1231231"
+            menu.style.display = 'none'
+        }, 300)
         isPaused = false
     }
 }
